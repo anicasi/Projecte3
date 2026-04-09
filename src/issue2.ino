e1053545316df8a9f7da03d98c1468a5724940a7
anicand <alexnicasi2@gmail.com>
2026-04-09 10:51:06 +0200

    Add web server functionality to ESP32

--- src/issue2.ino
+++ src/issue2.ino
@@ -18,6 +18,36 @@ while (WiFi.status() != WL_CONNECTED) {
 Serial.println("\n Connectat!");
 Serial.print("IP: ");
 Serial.println(WiFi.localIP());
+server.begin();
+server.on("/", enviarPaginaPrincipal);
+
+}
+
+void loop() {
+server.handleClient();
+}
+void enviarPaginaPrincipal() {
+  server.send(200, "text/plain", "Hola, soc el teu ESP32");
+}#include <WiFi.h>
+const char* ssid = "ProjectesSMX";
+const char* password = "Hola1234";
+
+#include <WebServer.h>
+WebServer server(80);
+
+
+void setup() {
+Serial.begin(115200);
+WiFi.begin(ssid, password);
+Serial.print("Connectant...");
+
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+}
+Serial.println("\n Connectat!");
+Serial.print("IP: ");
+Serial.println(WiFi.localIP());
   
 server.on("/", enviarPaginaPrincipal);
 server.begin();
