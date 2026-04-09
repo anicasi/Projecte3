b1e8f6958ecc1244d65331f304cd6da54d765c03
anicand <alexnicasi2@gmail.com>
2026-04-09 10:24:15 +0200

    Set up WiFi connection in issue1.ino
    Initialize WiFi connection with SSID and password.

--- src/issue1.ino
+++ src/issue1.ino
@@ -1,1 +1,23 @@
+#include <WiFi.h>
+const char* ssid = "ProjectesSMX";
+const char* password = "Hola1234";
 
+void setup() {
+Serial.begin(115200);
+WiFi.begin(ssid, password);
+Serial.print("Connectant...");
+
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+}
+Serial.println("\n Connectat!");
+Serial.print("IP: ");
+Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
