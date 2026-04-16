952fb444581ce0f40425aa1337b1fd6a180f77c2
anicand <alexnicasi2@gmail.com>
2026-04-16 09:54:42 +0200

    Initialize WiFi and web server for monitoring

--- src/main.ino
+++ src/main.ino
@@ -1,1 +1,42 @@
+#include <WiFi.h>
+const char* ssid = "ProjectesSMX";
+const char* password = "Hola1234";
+#include <WebServer.h>
+WebServer server(80);
+const int pinLDR = 35; 
+const int pinLM35 = 34;
 
+void setup() {
+Serial.begin(115200);
+WiFi.begin(ssid, password);
+Serial.print("Connectant...");
+
+while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  Serial.print(".");
+}
+Serial.println("\n Connectat!");
+Serial.print("IP: ");
+Serial.println(WiFi.localIP());
+server.begin();
+server.on("/", enviarPaginaPrincipal);
+server.on("/credits", credits);
+}
+
+void loop() {
+server.handleClient();
+}
+void enviarPaginaPrincipal() {
+int valorLDR = analogRead(pinLDR);
+float tempC = (analogRead(pinLM35) * 5.0 / 4095.0) * 100.0;
+
+  server.send(String html = "<html><head><meta charset='UTF-8'>";
+<meta http-equiv='refresh' content='5'>
+html += "<style>body{font-family:sans-serif; text-align:center;} .card{padding:20px; color:white; display:inline-block; margin:10px; border-radius:10px;}</style>";
+html += "</head><body><h1>Monitor CPD</h1>";
+html += "<div class='card' style='background:" + colorTemp + ";.'><h3>Temp</h3><p>String(tempC, 1) + "°C"</p></div>";
+html += "</body></html>";
+server.send(200, "text/html", html);
+);
+}
+void credits() { server.send(200, "text/html", "<h1>Fet per Alex i Jonathan</h1>"); }
