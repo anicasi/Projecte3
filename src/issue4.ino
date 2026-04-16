42c1dcba3f9b12e610c254584b8fb05d57a09372
anicand <alexnicasi2@gmail.com>
2026-04-16 10:32:47 +0200

    Update issue4.ino

--- src/issue4.ino
+++ src/issue4.ino
@@ -26,15 +26,11 @@ void loop() {
 server.handleClient();
 }
 void enviarPaginaPrincipal() {
-int valorLDR = analogRead(pinLDR);
-float tempC = (analogRead(pinLM35) * 5.0 / 4095.0) * 100.0;
-
-  server.send(String html = "<html><head><meta charset='UTF-8'>";
+String html = "<html><head><meta charset='UTF-8'>";
 <meta http-equiv='refresh' content='5'>
 html += "<style>body{font-family:sans-serif; text-align:center;} .card{padding:20px; color:white; display:inline-block; margin:10px; border-radius:10px;}</style>";
 html += "</head><body><h1>Monitor CPD</h1>";
 html += "<div class='card' style='background:gray;'><h3>Temp</h3><p>String(tempC, 1) + "°C"</p></div>";
 html += "</body></html>";
 server.send(200, "text/html", html);
-);
 }
