1985d10b7b6d9b12ad76829cec22af022d661774
anicand <alexnicasi2@gmail.com>
2026-04-16 10:41:20 +0200

    Update issue4.ino

--- src/issue4.ino
+++ src/issue4.ino
@@ -1,10 +1,12 @@
 #include <WiFi.h>
 const char* ssid = "ProjectesSMX";
 const char* password = "Hola1234";
+const int pinLDR = 35;
+const int pinLM35 = 34;
+
 #include <WebServer.h>
 WebServer server(80);
-const int pinLDR = 35; 
-const int pinLM35 = 34;
+
 
 void setup() {
 Serial.begin(115200);
@@ -26,11 +28,14 @@ void loop() {
 server.handleClient();
 }
 void enviarPaginaPrincipal() {
+int valorLDR = analogRead(pinLDR);
+float tempC = (analogRead(pinLM35) * 5.0 / 4095.0) * 100.0;
+
 String html = "<html><head><meta charset='UTF-8'>";
 <meta http-equiv='refresh' content='5'>
 html += "<style>body{font-family:sans-serif; text-align:center;} .card{padding:20px; color:white; display:inline-block; margin:10px; border-radius:10px;}</style>";
 html += "</head><body><h1>Monitor CPD</h1>";
-html += "<div class='card' style='background:gray;'><h3>Temp</h3><p>String(tempC, 1) + "°C"</p></div>";
+html += "<div class='card' style='background:gray;'><h3>Temp</h3><p>-- °C</p></div>";
 html += "</body></html>";
 server.send(200, "text/html", html);
 }
